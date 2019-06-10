c4d2710a312c33c7b3f342e8a910ce17d85e973a
Rick D <ikarago@outlook.com>
2019-06-10 19:33:28 +0200

    Base

---
+++ FluiFramework.UWP.Core/FluiFrameworkThemeResources.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace FluiFramework.UWP.Core
+{
+    public sealed class FluiFrameworkThemeResources : ResourceDictionary
+    {
+        public FluiFrameworkThemeResources()
+        {
+            // Regular styles
+            this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/_Thickness.xaml") });
+            this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/_Corners.xaml") });
+
+            // Control styles
+            this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/TextBlock.xaml") });
+            this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/TextBox.xaml") });
+        }
+    }
+}
--- FluiFramework.UWP.Core/Properties/AssemblyInfo.cs
+++ FluiFramework.UWP.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FluiFramework.UWP.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FluiFramework.UWP")]
+[assembly: AssemblyDescription("A NuGet package aimed to bring standardisation to Fluent Design by providing easy-to-use styles and controls")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FluiFramework.UWP.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Team FLUI")]
+[assembly: AssemblyProduct("FluiFramework.UWP")]
+[assembly: AssemblyCopyright("Copyright Team FLUI ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.20.0")]
+[assembly: AssemblyInformationalVersion("0.0.20-alpha")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
