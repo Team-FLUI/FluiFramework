a9acdd7c1281841e758049ec2c7f86b489cc1a9b
Rick D <ikarago@outlook.com>
2019-07-17 17:27:28 +0200

    Added Button Styles

--- FluiFramework.UWP.Core/FluiFrameworkThemeResources.cs
+++ FluiFramework.UWP.Core/FluiFrameworkThemeResources.cs
@@ -16,17 +16,20 @@ namespace FluiFramework.UWP.Core
             if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7))  // Minimal 1809
             {
                 this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/_Corners1809.xaml") });
+                this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/Button1809.xaml") });
                 this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/ComboBox1809.xaml") });
                 this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/TextBox1809.xaml") });
             }
             else // Pre-1809
             {
+                this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/Button.xaml") });
                 this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/ComboBox.xaml") });
                 this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/TextBox.xaml") });
             }
 
             // Compatible between all versions:
             // Regular Styles
+            this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/_Colours.xaml") });
             this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///FluiFramework.UWP.Core/Styles/_Thickness.xaml") });
 
             // Control Styles
