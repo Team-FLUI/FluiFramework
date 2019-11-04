5cd4b5230ebd985c6310a4320f50961af4438d61
Rick D <ikarago@outlook.com>
2019-11-04 14:49:45 +0100

    Implemented base dialog accent button (#5)

--- FluiFramework.UWP.Core/Properties/AssemblyInfo.cs
+++ FluiFramework.UWP.Core/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.31.0")]
-[assembly: AssemblyInformationalVersion("0.0.31")]
+[assembly: AssemblyInformationalVersion("0.0.32")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
